e784597742cd36cd0a74eee65eafcd064f6d3f35
Christopher Su <christophersu9@gmail.com>
2015-03-24 03:53:55 -0700

    results working

--- js/controllers.js
+++ js/controllers.js
@@ -8,15 +8,59 @@ compatApp.controller('CompatCtrl', function ($scope, $http) {
 
     $scope.person = {};
 
-    $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+    // Use x-www-form-urlencoded Content-Type
+    $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+
+    // Override $http service's default transformRequest
+    $http.defaults.transformRequest = [function(data) {
+        return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+    }];
+
     $scope.submitMainForm = function() {
         $http.post('/api/compute', {
                     me: $scope.person.first,
                     friend: $scope.person.second
         }).
         success(function(data, status, headers, config) {
-            $layout.mainContent = "/static/html/results.html";
+            $scope.result = data;
+            $scope.layout.mainContent = "/static/html/results.html";
         }).
         error(function(data, status, headers, config) {});
-    }
+    };
 });
+
+/**
+   * The workhorse; converts an object to x-www-form-urlencoded serialization.
+   * @param {Object} obj
+   * @return {String}
+   */ 
+  var param = function(obj) {
+    var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
+      
+    for(name in obj) {
+      value = obj[name];
+        
+      if(value instanceof Array) {
+        for(i=0; i<value.length; ++i) {
+          subValue = value[i];
+          fullSubName = name + '[' + i + ']';
+          innerObj = {};
+          innerObj[fullSubName] = subValue;
+          query += param(innerObj) + '&';
+        }
+      }
+      else if(value instanceof Object) {
+        for(subName in value) {
+          subValue = value[subName];
+          fullSubName = name + '[' + subName + ']';
+          innerObj = {};
+          innerObj[fullSubName] = subValue;
+          query += param(innerObj) + '&';
+        }
+      }
+      else if(value !== undefined && value !== null)
+        query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+    }
+      
+    return query.length ? query.substr(0, query.length - 1) : query;
+  };
