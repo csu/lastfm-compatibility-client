1e71b4295a9250ffb3366b0ca4be93562313f3ab
Christopher Su <christophersu9@gmail.com>
2015-03-24 03:36:51 -0700

    fml

--- js/controllers.js
+++ js/controllers.js
@@ -8,10 +8,16 @@ compatApp.controller('CompatCtrl', function ($scope, $http) {
 
     $scope.person = {};
 
+    $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
     $scope.submitMainForm = function() {
-        $http.post('/api/compute', {
-            me: $scope.person.first,
-            friend: $scope.person.second
+        $http({
+            url: '/api/compute',
+            method: "POST",
+            data: JSON.stringify({
+                    me: $scope.person.first,
+                    friend: $scope.person.second
+            }),
+            headers: {'Content-Type': 'application/json'}
         }).
         success(function(data, status, headers, config) {
             $layout.mainContent = "/static/html/results.html";
