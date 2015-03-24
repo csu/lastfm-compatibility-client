44bd207477d7018ce8cf725c5cc67492653d077b
Christopher Su <christophersu9@gmail.com>
2015-03-24 00:36:20 -0700

    js

--- js/controllers.js
+++ js/controllers.js
@@ -3,5 +3,19 @@
 var compatApp = angular.module('compatApp', []);
 
 compatApp.controller('CompatCtrl', function ($scope, $http) {
-    
+    $scope.layout = {};
+    $scope.layout.mainContent = "/static/html/home-form.html";
+
+    $scope.person = {};
+
+    $scope.submitMainForm = function() {
+        $http.post('/api/compute', {
+            me: $scope.person.first,
+            friend: $scope.person.second
+        }).
+        success(function(data, status, headers, config) {
+            $layout.mainContent = "/static/html/results.html";
+        }).
+        error(function(data, status, headers, config) {});
+    }
 });
